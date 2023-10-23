97ceb4bf06b1f5c4baabe339b63381debaec2d85
ccckmit <ccckmit@gmail.com>
2023-10-23 11:44:44 +0800

    schedule pass

--- prototype/04-schedule/schedule.py
+++ prototype/04-schedule/schedule.py
@@ -1,15 +1,35 @@
 import os
 import openai
+import json
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-MEMORY = """
-# 陳鍾誠的行事紀錄
+BACKGROUND = """
+我是陳鍾誠，你是我的秘書，必須要負責記錄我的行事曆
 
-陳鍾誠:2023/10/01 12:35 本月 22 日中午我有系務會議要開
-陳鍾誠:2023/10/02 14:03 本月 10 日放假一天
-陳鍾誠:2023/10/07 15:22 我 11 月 15 號我要飛泰國，坐華航下午 3:20 的班機去
-陳鍾誠:2023/10/13 09:22 明天下午三點接小孩去補習
+例如： 2023/10/1 的陳鍾誠說：我後天下午三點要搭華航飛機去泰國
+
+這時請傳回 {"分類":"行事曆",  "日期":"2023/10/3 15:00", "任務":"搭華航飛機去泰國")
+
+注意，2023/10/1 所說的後天下午三點，應該是 2023/10/3 15:00，日期時間必須明確化表達
+
+現在開始我的問題，你都要這樣將時間明確化進行改寫
+
+然後在我問你哪天有甚麼事情要做時，告訴我那天該做甚麼事情
+"""
+
+jobs = [
+"2023/10/01 12:35 本月 22 日中午我有系務會議要開",
+"2023/10/07 15:22 我下個月 15 號我要飛泰國，坐華航下午 3:20 的班機去",
+]
+
+"""
+jobs = [
+"2023/10/01 12:35 本月 22 日中午我有系務會議要開",
+"2023/10/02 14:03 本月 10 日放假一天",
+"2023/10/07 15:22 我下個月 15 號我要飛泰國，坐華航下午 3:20 的班機去",
+"2023/10/13 09:22 明天下午三點接小孩去補習",
+]
 """
 
 def chat(q):
@@ -17,12 +37,21 @@ def chat(q):
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are a chatbot"},
-            {"role": "user", "content": f"{MEMORY}\n\n{q}"}
+            {"role": "assistant", "content": f"{BACKGROUND}"},
+            {"role": "user", "content": f"{q}"}
         ]
     )
-    print('memory=', MEMORY)
-    print(response)
+    # print(response)
     return response['choices'][0]['message']['content']
 
-r = chat('請問陳鍾誠 2023/11/15 日有甚麼事要做呢？')
+schedules = []
+
+for job in jobs:
+    r = chat(job)
+    print(r)
+    task = json.loads(r)
+    task['原文'] = job
+    schedules.append(task)
+
+r = chat(f'{schedules}\n請問我 2023/11/15 日有甚麼事要做呢？')
 print(r)
